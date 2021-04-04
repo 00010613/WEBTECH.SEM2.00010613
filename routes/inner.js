6ee71ecff328824c4e4b71e5d881d65c13238aa8
UmiTello <student.00010613@gmail.com>
2021-04-05 02:47:35 +0500

    additions-singlepostshow

--- routes/inner.js
+++ routes/inner.js
@@ -1,8 +1,20 @@
 const express = require("express")
+const path = require('path')
+const fs = require('fs')
+const DB = path.resolve(__dirname, '../data/posts.json')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('inner')
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile(DB, (err, data) => {
+        if (err) throw err
+
+        const posts_list = JSON.parse(data)
+        const post = posts_list.filter(post => post.id == id)[0]
+
+        res.render('inner', { post: post })
+    })
 })
 
 module.exports = router;
