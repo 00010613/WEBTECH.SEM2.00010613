9fd46c64a66fcf6f7f7e2cb860b8020ae6b40058
UmiTello <student.00010613@gmail.com>
2021-04-05 01:20:24 +0500

    addition-routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 8000
 const express = require('express')
 const app = express()
 const fs = require('fs')
@@ -5,33 +6,31 @@ const fs = require('fs')
 app.set('view engine', 'pug')
 const DB = './data/posts.json'
 
+// ROUTES
+const mainRouter = require("./routes/index.js");
+const aboutRouter = require("./routes/about.js");
+const allpostsRouter = require("./routes/allposts.js");
+const contactRouter = require("./routes/contact.js");
+const innerRouter = require("./routes/inner.js");
+const newpostRouter = require("./routes/newpost.js");
+
+
+
 app.use('/static/styles', express.static('public/styles'))
 app.use('/static/img', express.static('public/img'))
 app.use('/static/icons', express.static('public/icons'))
 app.use('/static/fonts', express.static('public/fonts'))
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-app.get('/allposts', (req, res) => {
-    res.render('allposts')
-})
-app.get('/contact', (req, res) => {
-    res.render('contact')
-})
-app.get('/inner', (req, res) => {
-    res.render('inner')
-})
-app.get('/newpost', (req, res) => {
-    res.render('newpost')
-})
 
-app.listen(8000, err => {
-    if (err) throw err
+app.use('/', mainRouter)
+app.use('/about', aboutRouter)
+app.use('/allposts', allpostsRouter)
+app.use('/contact', contactRouter)
+app.use('/inner', innerRouter)
+app.use('/newpost', newpostRouter)
 
-    console.log('App is running on port 8000...')
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}...`)
 })
